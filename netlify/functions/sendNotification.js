faf45467bbfc75b1e3b18c242297c27ee9ac7ba4
Pedro Martins <mourao.martins@gmail.com>
2025-04-28 23:04:03 +0100

    Add files via upload

--- netlify/functions/sendNotification.js
+++ netlify/functions/sendNotification.js
@@ -1,8 +1,8 @@
 const webpush = require('web-push');
 
 const vapidKeys = {
-  publicKey: 'BFIKfwEcsb9Uguw0v2z1oOfVgE0lsSOPqkFq66Xz9Vq4W2eO7EK5gGxHDIWYl0QYwRg-M8FcEUNe5IXwx4txZGc',
-  privateKey: 'mSkEVhLQw5SlN4gc1c4cMxXLoC6FYOehG_J8qtaEec4'
+  publicKey: 'BFA3rK2osEArdTWujd9Xss9e7X8OT2LJuGHj9TnVnUQYUpYtlfq-M7qkPUg9OCfWoxgPV6pE8Zpa7AztvT2xZSo',
+  privateKey: 'OxOd4j3GyGvA7fjKblT6TJSPjwogZdT7pV2FV7sm82E'
 };
 
 webpush.setVapidDetails(
@@ -16,7 +16,7 @@ exports.handler = async (event) => {
 
   const payload = JSON.stringify({
     title: 'Nova Notificação!',
-    body: 'Enviada sem Firebase, 100% controlada por ti!'
+    body: 'Esta é uma notificação Web Push sem Firebase!'
   });
 
   try {
