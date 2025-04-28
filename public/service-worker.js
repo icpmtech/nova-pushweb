a5ae1234cc699080b597c36765dfb9151f5418c4
Pedro Martins <mourao.martins@gmail.com>
2025-04-28 22:53:25 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,3 +21,35 @@ self.addEventListener('notificationclick', event => {
     clients.openWindow(event.notification.data.url)
   );
 });
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Instalação feita');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Ativado');
+  return self.clients.claim();
+});
+
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  console.log('Push recebido', data);
+
+  const options = {
+    body: data.body,
+    icon: '/icon.png',
+    badge: '/icon.png',
+    data: { url: 'https://www.google.com' }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
