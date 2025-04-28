5cb59ad0b3f5f11c2e68ffeb393e140b23aa2cf6
Pedro Martins <mourao.martins@gmail.com>
2025-04-28 23:03:40 +0100

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,5 @@
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  console.log('Push recebido', data);
-
-  const options = {
-    body: data.body,
-    icon: '/icon.png',
-    data: {
-      url: 'https://www.google.com'
-    }
-  };
-
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
-  );
-});
-
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(event.notification.data.url)
-  );
-});
 self.addEventListener('install', event => {
-  console.log('[Service Worker] Instalação feita');
+  console.log('[Service Worker] Instalado');
   self.skipWaiting();
 });
 
